46f1a0b3f38528239298de62b821b1717cd18b53
Ludwigson Morisseau <morisseau@Ludwigsons-MacBook-Air.local>
2022-05-23 19:25:20 -0400

    created a static html & css menu page

---
+++ script.js
@@ -0,0 +1,20 @@
+const menuElement = document.querySelector('#menu')
+
+fetch('https://codice-boca.web.app/menu')
+.then(response => response.json()) // getting raw data
+.then(cleanData => {
+    const menuCards = cleanData.map((menuItem) => {
+        console.log('menuItem here', menuItem)
+        
+        return `<div class="menu-item">
+        <h3>${menuItem.title}</h3>
+        <p>${menuItem.description}</p>
+      </div>`
+    })
+
+menuElement.innerHTML = menuCards.join('')
+}) // logging items from json
+.catch(err => console.error(err))
+
+
+
